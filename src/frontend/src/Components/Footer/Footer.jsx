fe00e63817e3ac4a77464a44c7839d69ad56e465
allisonvinicius <allisonvinicius63@gmail.com>
2022-03-13 19:15:04 -0400

    updates

---
+++ src/frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+           <span>
+               Desenvolvido com <i className="fa fa-heart text-danger"></i>
+               por <strong> Allison <span className="text-danger">Vinicius</span>..'</strong>
+           </span>
+        </footer>
+    );
+}
+export default Footer;
+
+
