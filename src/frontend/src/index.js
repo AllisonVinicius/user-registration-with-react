cfba8200704d5d69007030171e53fdb9e2f0da21
allisonvinicius <allisonvinicius63@gmail.com>
2022-03-13 19:15:29 -0400

    updates in component all

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './main/App';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 
-reportWebVitals();
