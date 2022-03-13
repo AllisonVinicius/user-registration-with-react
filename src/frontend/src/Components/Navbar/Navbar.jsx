31d58e1dd29d2a72a69ee3aa37d40a8e5e74db58
allisonvinicius <allisonvinicius63@gmail.com>
2022-03-13 19:55:14 -0400

    navbar component

--- src/frontend/src/Components/Navbar/Navbar.jsx
+++ src/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import './Navbar.css';
-
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <aside className='menu-area'>
-            <nav className='menu'>
-                <a href='/'>
-                    <i className='fa fa-home'></i>
-                    Inicio
-                </a>
-                <a href='#/users'>
-                    <i className='fa fa-users'></i>
-                    Usuários
-                </a>
-            </nav>
-        
-        </aside>
-
-    )
+  return (
+    <aside className="menu-area">
+      <nav className="menu">
+        <a href="/">
+          <i className="fa fa-home"></i>
+          Início
+        </a>
+        <a href="#/users">
+          <i className="fa fa-users"></i>
+          Usuários
+        </a>
+      </nav>
+    </aside>
+  );
 };
 export default Navbar;
